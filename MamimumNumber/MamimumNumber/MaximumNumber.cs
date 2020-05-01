bc181a65721e6a57b89202e5744663ad45338abc
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-05-01 10:26:51 +0530

    Added: Find the maximum Integer number

---
+++ MamimumNumber/MamimumNumber/MaximumNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MamimumNumber
+{
+    class MaximumNumber
+    {
+        //Find the Maximun Integer Number
+        public int maximumIntegerNumber(int firstValue, int secondValue, int thirdValue)
+        {
+            //Comparing firstvalue with second and third value
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            //Comparing secondvalue with first and third value
+            else if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            //Comparing thirdvalue with first and second value
+            else if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
