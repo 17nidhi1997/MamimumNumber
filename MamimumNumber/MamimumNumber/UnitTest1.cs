9821b56d0c1dcfd1c3de2448bc66d206cb2b54ac
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-05-01 10:35:32 +0530

    Added: Testing the maximum Integer number at second place

--- MamimumNumber/MamimumNumber/UnitTest1.cs
+++ MamimumNumber/MamimumNumber/UnitTest1.cs
@@ -11,5 +11,12 @@ namespace MamimumNumber
             int actualMax = md.maximumIntegerNumber(20, 15, 14);
             Assert.AreEqual(20, actualMax);
         }
+        [Test]
+        public void MaxIntegerValue_SecondValue()
+        {
+            MaximumNumber md = new MaximumNumber();
+            int actualMax = md.maximumIntegerNumber(10, 15, 14);
+            Assert.AreEqual(15, actualMax);
+        }
     }
 }
