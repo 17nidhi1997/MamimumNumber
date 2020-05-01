67e8066d05930ad6d17e4bc417193c2167cdb9f8
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-05-01 13:42:48 +0530

    Refactor: added Generic Type class to take in 3 variable of generic type

--- MamimumNumber/MamimumNumber/MaximumNumber.cs
+++ MamimumNumber/MamimumNumber/MaximumNumber.cs
@@ -5,18 +5,26 @@ using System.Text;
 
 namespace MamimumNumber
 {
-   class MaximumNumber
+    class MaximumNumber<GTYPE>
     {
-          //Generic method for all kinds of data to pass
-            public GTYPE maxValueArray<GTYPE>(GTYPE firstValue, GTYPE secondValue, GTYPE thirdValue)
-            {
-                GTYPE[] maxNumber = { firstValue, secondValue, thirdValue };
-                //Finding The Maximum GTYPE Array[integer,double,string]
-                GTYPE MaxValue = maxNumber.Max();
-                return MaxValue;
-            }
-
-
-        
+        GTYPE firstValue, secondValue, thirdValue;
+        //Generic method for all kinds of data to pass
+        public MaximumNumber(GTYPE firstValue, GTYPE secondValue, GTYPE thirdValue)
+        {
+            this.firstValue = firstValue;
+            this.secondValue = secondValue;
+            this.thirdValue = thirdValue;
+        }
+        public GTYPE TestMaximum()
+        {
+            return maxValueArray(firstValue, secondValue, thirdValue);
+        }
+        public static GTYPE maxValueArray(GTYPE firstValue, GTYPE secondValue, GTYPE thirdValue)
+        {
+            GTYPE[] maxNumber = { firstValue, secondValue, thirdValue };
+            //Finding The Maximum GTYPE Array[integer,double,string]
+            GTYPE MaxValue = maxNumber.Max();
+            return MaxValue;
+        }
     }
 }
