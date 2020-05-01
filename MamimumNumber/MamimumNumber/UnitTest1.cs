2fd3648138bd3dc3579cb9d53545a5b4e20f4e24
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-05-01 13:44:12 +0530

    Refactor: Testing Generic Type class to take in 3 variable of generic type

--- MamimumNumber/MamimumNumber/UnitTest1.cs
+++ MamimumNumber/MamimumNumber/UnitTest1.cs
@@ -5,69 +5,69 @@ namespace MamimumNumber
     [TestFixture]
     public class Tests
     {
-        
+
         [Test]
-         public void MaxIntegerValue_FirstValue()
-         {
-             MaximumNumber md = new MaximumNumber();
-             int actualMax = md.maxValueArray(20, 15, 14);
-             Assert.AreEqual(20, actualMax);
-         }
-         [Test]
-         public void MaxIntegerValue_SecondValue()
-         {
-             MaximumNumber md = new MaximumNumber();
-             int actualMax = md.maxValueArray(10, 15, 14);
-             Assert.AreEqual(15, actualMax);
-         }
-         [Test]
-         public void MaxIntegerValue_ThirdValue()
-         {
-             MaximumNumber md = new MaximumNumber();
-             int actualMax = md.maxValueArray(10, 15, 20);
-             Assert.AreEqual(20, actualMax);
-         }
-         [Test]
-         public void MaxDoubleValue_FirstValue()
-         {
-             MaximumNumber md = new MaximumNumber();
-             double actualMax = md.maxValueArray(20.5, 15.2, 14.3);
-             Assert.AreEqual(20.5, actualMax);
-         }
-         [Test]
-         public void MaxDoubleValue_secondValue()
-         {
-             MaximumNumber md = new MaximumNumber();
-             double actualMax = md.maxValueArray(10.5, 15.2, 14.3);
-             Assert.AreEqual(15.2, actualMax);
-         }
-         [Test]
-         public void MaxDoubleValue_ThirdValue()
-         {
-             MaximumNumber md = new MaximumNumber();
-             double actualMax = md.maxValueArray(10.5, 15.2, 20.3);
-             Assert.AreEqual(20.3, actualMax);
-         }
-         [Test]
-         public void MaxStringValue_FirstValue()
-         {
-             MaximumNumber md = new MaximumNumber();
-             string actualMax = md.maxValueArray("peach", "apple", "banana");
-             Assert.AreEqual("peach", actualMax);
-         }
-         [Test]
-         public void MaxStringValue_secondValue()
-         {
-             MaximumNumber md = new MaximumNumber();
-             string actualMax = md.maxValueArray("apple", "peach", "banana");
-             Assert.AreEqual("peach", actualMax);
-         }
-         [Test]
-         public void MaxStringValue_ThirdValue()
-         {
-             MaximumNumber md = new MaximumNumber();
-             string actualMax = md.maxValueArray("apple", "banana", "peach");
-             Assert.AreEqual("peach", actualMax);
-         }
+        public void MaxIntegerValue_FirstValue()
+        {
+
+            int actualMax = MaximumNumber<int>.maxValueArray(20, 15, 14);
+            Assert.AreEqual(20, actualMax);
+        }
+        [Test]
+        public void MaxIntegerValue_SecondValue()
+        {
+
+            int actualMax = MaximumNumber<int>.maxValueArray(10, 15, 14);
+            Assert.AreEqual(15, actualMax);
+        }
+        [Test]
+        public void MaxIntegerValue_ThirdValue()
+        {
+
+            int actualMax = MaximumNumber<int>.maxValueArray(10, 15, 20);
+            Assert.AreEqual(20, actualMax);
+        }
+        [Test]
+        public void MaxDoubleValue_FirstValue()
+        {
+
+            double actualMax = MaximumNumber<double>.maxValueArray(20.5, 15.2, 14.3);
+            Assert.AreEqual(20.5, actualMax);
+        }
+        [Test]
+        public void MaxDoubleValue_secondValue()
+        {
+
+            double actualMax = MaximumNumber<double>.maxValueArray(10.5, 15.2, 14.3);
+            Assert.AreEqual(15.2, actualMax);
+        }
+        [Test]
+        public void MaxDoubleValue_ThirdValue()
+        {
+
+            double actualMax = MaximumNumber<double>.maxValueArray(10.5, 15.2, 20.3);
+            Assert.AreEqual(20.3, actualMax);
+        }
+        [Test]
+        public void MaxStringValue_FirstValue()
+        {
+
+            string actualMax = MaximumNumber<string>.maxValueArray("peach", "apple", "banana");
+            Assert.AreEqual("peach", actualMax);
+        }
+        [Test]
+        public void MaxStringValue_secondValue()
+        {
+
+            string actualMax = MaximumNumber<string>.maxValueArray("apple", "peach", "banana");
+            Assert.AreEqual("peach", actualMax);
+        }
+        [Test]
+        public void MaxStringValue_ThirdValue()
+        {
+
+            string actualMax = MaximumNumber<string>.maxValueArray("apple", "banana", "peach");
+            Assert.AreEqual("peach", actualMax);
+        }
     }
 }
